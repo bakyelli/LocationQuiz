f3f00103a31d7e148bf7f9eed3d3caec0eb0b512
James Lin <jameslin101@gmail.com>
2013-10-25 15:51:13 -0400

    add drawer

---
+++ LocationQuiz/LocationQuiz/DrawerTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DrawTableViewController.h
+//  LocationQuiz
+//
+//  Created by Chemin Lin on 10/25/13.
+//  Copyright (c) 2013 Jay Abdallah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DrawTableViewController : UIViewController
+
+@end
---
+++ LocationQuiz/LocationQuiz/DrawerTableViewController.m
@@ -0,0 +1,38 @@
+//
+//  DrawTableViewController.m
+//  LocationQuiz
+//
+//  Created by Chemin Lin on 10/25/13.
+//  Copyright (c) 2013 Jay Abdallah. All rights reserved.
+//
+
+#import "DrawTableViewController.h"
+
+@interface DrawTableViewController ()
+
+@end
+
+@implementation DrawTableViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
